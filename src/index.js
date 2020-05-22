86f76433eae37451c29a7e3281a2dd8fb7b3325e
dantegg <dantegg327@gmail.com>
2020-05-22 16:15:58 +0800

    update

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 var PROTO_PATH = __dirname + '/proto/';
 var grpc = require("grpc");
-var hello_proto = grpc.load(PROTO_PATH).helloworld;
+var hello_proto = grpc.load(PROTO_PATH).helloWorld;
 
 console.log("path is " + __dirname);
 console.log(PROTO_PATH);
+
 
