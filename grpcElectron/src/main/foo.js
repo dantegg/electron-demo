1c6a6f4c2f5db4354a636727c1df486e012f5f78
dantegg <dantegg327@gmail.com>
2020-05-25 16:43:49 +0800

    update

--- grpcElectron/src/main/foo.js
+++ grpcElectron/src/main/foo.js
@@ -1,5 +1,13 @@
+var fs = require('fs');
 module.exports = {
     getTest() {
         console.log("test!!!");
+        fs.writeFile('./test.md', 'this is a demo test', function(error) {
+            if(error) {
+                console.log('fail');
+            } else {
+                console.log('success');
+            }
+        });
     }
 }
