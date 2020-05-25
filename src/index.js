b4de31627164c8710b42c266591f28db1a1bccf0
dantegg <dantegg327@gmail.com>
2020-05-25 14:00:25 +0800

    update

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
-var PROTO_PATH = __dirname + '/proto/';
-var grpc = require("grpc");
-var hello_proto = grpc.load(PROTO_PATH).helloWorld;
+var PROTO_PATH = __dirname + '/proto/'
+var grpc = require('grpc')
+var hello_proto = grpc.load(PROTO_PATH).helloWorld
 
-console.log("path is " + __dirname);
-console.log(PROTO_PATH);
-
-
+console.log('path is ' + __dirname)
+console.log(PROTO_PATH)
--- src/server.js
+++ src/server.js
@@ -1,23 +1,23 @@
 // 定义proto接口描述文件路径
-var PROTO_PATH = __dirname + '/proto/helloworld.proto';
+var PROTO_PATH = __dirname + '/proto/helloworld.proto'
 // 加载grpc模块
-var grpc = require('grpc');
+var grpc = require('grpc')
 // 加载proto文件中helloworld包的配置
-var hello_proto = grpc.load(PROTO_PATH).helloworld;
+var hello_proto = grpc.load(PROTO_PATH).helloworld
 // 定义sayHello方法
-function sayHello(call, callback) {
-  callback(null, {message: 'Node.js: ' + call.request.name});
+function sayHello (call, callback) {
+  callback(null, {message: 'Node.js: ' + call.request.name})
 }
 // 主函数
-function main() {
+function main () {
   // 实例grpc服务
-  var server = new grpc.Server();
+  var server = new grpc.Server()
   // 注册Greeter服务到grpc服务，并定义接口对应方法
-  server.addService(hello_proto.Greeter.service, {sayHello: sayHello});
+  server.addService(hello_proto.Greeter.service, {sayHello: sayHello})
   // 绑定地址，并传入证书(这里是忽略验证)
-  server.bind('0.0.0.0:50051', grpc.ServerCredentials.createInsecure());
- // 启动服务
-  server.start();
+  server.bind('0.0.0.0:50051', grpc.ServerCredentials.createInsecure())
+  // 启动服务
+  server.start()
 }
 
-main();
+main()
