f69f9e3189d6840de4d4c249d9838a00691c910b
dantegg <dantegg327@gmail.com>
2020-05-28 10:04:56 +0800

    update grpc client

--- grpcElectron/src/main/grpcClient.js
+++ grpcElectron/src/main/grpcClient.js
@@ -1,15 +1,23 @@
-'use strict';
+var grpc = require('grpc');
+const path = require('path');
 
-import grpc from 'grpc';
+const PROTO_PATH = path.resolve(__dirname, '../../protos/helloworld.proto');
+console.log("grpc proto : " + PROTO_PATH);
 
-const PROTO_PATH = __dirname + '/protos/helloworld.proto';
+const testProto = grpc.load(PROTO_PATH).helloworld;
 
-const testProto = grpc.load(PROTO_PATH).testPackage
+const client = new testProto.Greeter('0.0.0.0:50051', grpc.credentials.createInsecure());
+
+client.SayHello({name:'test', age:11}, function(err, response) {
+    console.log("result is ->", response);
+})
+
+// const testProto = grpc.load(PROTO_PATH).testPackage
  
-const client = new testProto.testService('0.0.0.0:50051',
-                                       grpc.credentials.createInsecure());
+// const client = new testProto.testService('0.0.0.0:50051',
+//                                        grpc.credentials.createInsecure());
  
-client.ping({}, function(err, response) {
-    console.log('ping -> :', response.message);
-});
+// client.ping({}, function(err, response) {
+//     console.log('ping -> :', response.message);
+// });
 
